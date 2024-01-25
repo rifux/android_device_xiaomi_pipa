6459f0b1a2859ccc1b51dcc461cbbe0249a72d09
Abdulwahab Isam <abdoi94.iq@gmail.com>
2024-01-25 17:04:46 +0300

    Adapt to LMODroid
    Signed-off-by: Abdulwahab Isam <abdoi94.iq@gmail.com>
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_pipa.mk
+    $(LOCAL_DIR)/lmodroid_pipa.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_pipa-user \
-    lineage_pipa-userdebug \
-    lineage_pipa-eng
+    lmodroid_pipa-user \
+    lmodroid_pipa-userdebug \
+    lmodroid_pipa-eng
